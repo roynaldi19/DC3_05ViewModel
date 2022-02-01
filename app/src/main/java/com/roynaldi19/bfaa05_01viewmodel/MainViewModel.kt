78a8aa262e0cafcdc8a3bbfd01d1a813f366815a
roynaldi19 <roynaldi19@gmail.com>
2022-02-01 11:34:47 +0700

    Initial commit

---
+++ app/src/main/java/com/roynaldi19/bfaa05_01viewmodel/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.roynaldi19.bfaa05_01viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    var result = 0
+
+    fun calculate(width: String, height: String, length: String) {
+        result = width.toInt() * height.toInt() * length.toInt()
+    }
+}
